860be2429279f005d3ddf6190f9f914fc5f23cb3
Mitchell Kotler <mitch@mitchellkotler.net>
2018-08-01 11:18:04 -0400

    Do not allow unicode in usernames

--- squarelet/users/validators.py
+++ squarelet/users/validators.py
@@ -5,14 +5,17 @@ from django.core import validators
 from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
+# Standard Library
+import re
+
 
 @deconstructible
 class UsernameValidator(validators.RegexValidator):
     """The standard username validator, except disallowing @ symbols"""
 
-    regex = r"^[\w.+-]+$"
+    regex = r"^[\w.-]+$"
     message = _(
-        "Enter a valid username. This value may contain only letters, "
-        "numbers, and ./+/-/_ characters."
+        "Enter a valid username. This value may contain only English letters, "
+        "numbers, and ./-/_ characters."
     )
-    flags = 0
+    flags = re.ASCII
