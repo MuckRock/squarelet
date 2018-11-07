c73edd472e27880c60d421364d23fa097e7b1cd0
Mitchell Kotler <mitch@mitchellkotler.net>
2018-11-07 11:11:19 -0500

    create individual org on account creation

--- squarelet/users/adapters.py
+++ squarelet/users/adapters.py
@@ -1,9 +1,13 @@
 # Django
 from django.conf import settings
+from django.db import transaction
 
 # Third Party
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+# Squarelet
+from squarelet.organizations.models import Membership, Organization
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -12,8 +16,19 @@ class AccountAdapter(DefaultAccountAdapter):
 
     def save_user(self, request, user, form, commit=True):
         """Save the user's full name"""
-        user.name = form.cleaned_data.get("name", "")
-        super().save_user(request, user, form, commit)
+        with transaction.atomic():
+            user.name = form.cleaned_data.get("name", "")
+            user = super().save_user(request, user, form, commit)
+            # XXX validate things here - ie ensure name uniqueness
+            organization = Organization.objects.create(
+                id=user.pk,
+                name=user.username,
+                individual=True,
+                private=True,
+                max_users=1,
+            )
+            Membership.objects.create(user=user, organization=organization, admin=True)
+        return user
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
