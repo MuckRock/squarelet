c534af4c43abda181f56998036797e193363baec
Mitchell Kotler <mitch@mitchellkotler.net>
2019-01-31 13:10:48 -0500

    remove about page

--- config/urls.py
+++ config/urls.py
@@ -21,9 +21,6 @@ router.register("charges", ChargeViewSet)
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
-    path(
         "selectplan/",
         TemplateView.as_view(template_name="pages/selectplan.html"),
         name="select_plan",
