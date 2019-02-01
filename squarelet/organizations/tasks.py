6d2e4f9b5a1a9e1f21e70349154c7c7b6c74dded
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-01 13:46:40 -0500

    renamedate_update to update_on

--- squarelet/organizations/tasks.py
+++ squarelet/organizations/tasks.py
@@ -18,10 +18,10 @@ from .models import Charge, Organization, Plan
 @periodic_task(run_every=crontab(hour=0, minute=5), name="restore_organizations")
 def restore_organization():
     """Monthly update of organizations subscriptions"""
-    organizations = Organization.objects.filter(date_update__lte=date.today())
+    organizations = Organization.objects.filter(update_on__lte=date.today())
     uuids = organizations.values_list("pk", flat=True)
     organizations.update(
-        date_update=date.today() + Interval("1 month"), plan=F("next_plan")
+        update_on=date.today() + Interval("1 month"), plan=F("next_plan")
     )
     # XXX send single cache invalidation for all uuids
     for uuid in uuids:
