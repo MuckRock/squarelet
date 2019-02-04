241edb250daca3884f2ccbe23ae9184fc7355a0b
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-04 16:45:09 -0500

    redo handleintent with an ordereddict

--- squarelet/core/templatetags/handleintent.py
+++ squarelet/core/templatetags/handleintent.py
@@ -2,6 +2,9 @@
 from django import template
 from django.utils.translation import ugettext_lazy as _
 
+# Standard Library
+from collections import OrderedDict
+
 register = template.Library()
 
 # Service names and assets pointing to their logos
@@ -17,16 +20,6 @@ FOIAMACHINE_ASSET = "assets/foiamachine.svg"
 QUACKBOT_SERVICE = "Quackbot"
 QUACKBOT_ASSET = "assets/quackbot.svg"
 
-# Services and assets in order of display priority
-# XXX define these in terms of one ordered dict
-ALL_SERVICES = [
-    MUCKROCK_SERVICE,
-    DOCUMENTCLOUD_SERVICE,
-    FOIAMACHINE_SERVICE,
-    QUACKBOT_SERVICE,
-]
-ALL_ASSETS = [MUCKROCK_ASSET, DOCUMENTCLOUD_ASSET, FOIAMACHINE_ASSET, QUACKBOT_ASSET]
-
 
 @register.inclusion_tag("templatetags/intent.html", takes_context=True)
 def handleintent(context, header, message):
@@ -35,18 +28,20 @@ def handleintent(context, header, message):
     else:
         intent = "muckrock"
 
-    intent_lookup = {
-        "documentcloud": (DOCUMENTCLOUD_SERVICE, DOCUMENTCLOUD_ASSET),
-        "foiamachine": (FOIAMACHINE_SERVICE, FOIAMACHINE_ASSET),
-        "quackbot": (QUACKBOT_SERVICE, QUACKBOT_ASSET),
-        "muckrock": (MUCKROCK_SERVICE, MUCKROCK_ASSET),
-    }
+    intent_lookup = OrderedDict(
+        [
+            ("muckrock", (MUCKROCK_SERVICE, MUCKROCK_ASSET)),
+            ("documentcloud", (DOCUMENTCLOUD_SERVICE, DOCUMENTCLOUD_ASSET)),
+            ("foiamachine", (FOIAMACHINE_SERVICE, FOIAMACHINE_ASSET)),
+            ("quackbot", (QUACKBOT_SERVICE, QUACKBOT_ASSET)),
+        ]
+    )
 
     # default to 'muckrock'
     intent_service, intent_asset = intent_lookup.get(intent, intent_lookup["muckrock"])
 
-    other_services = [s for s in ALL_SERVICES if s != intent_service]
-    other_assets = [a for a in ALL_ASSETS if a != intent_asset]
+    other_services = [s for s, a in intent_lookup.values() if s != intent_service]
+    other_assets = [a for s, a in intent_lookup.values() if a != intent_asset]
 
     service_message = _("{}, and {}").format(
         ", ".join(other_services[:-1]), other_services[-1]
