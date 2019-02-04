2396b8512e1a4a1607e8a3b6425c6c86613a9ec1
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-04 10:22:07 -0500

    payment failed notifications

--- squarelet/core/context_processors.py
+++ squarelet/core/context_processors.py
@@ -4,3 +4,13 @@ from django.conf import settings as django_settings
 
 def settings(request):
     return {"settings": django_settings}
+
+
+def payment_failed(request):
+    if request.user.is_authenticated:
+        payment_failed_organizations = request.user.organizations.filter(
+            memberships__admin=True, payment_failed=True
+        )
+    else:
+        payment_failed_organizations = None
+    return {"payment_failed_organizations": payment_failed_organizations}
