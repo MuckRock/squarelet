cdcfa49ae0018854cdf7e24a83ea1674f3689ad5
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-06 14:14:18 -0500

    send cache invalidations on email change

--- squarelet/users/signals.py
+++ squarelet/users/signals.py
@@ -1,18 +1,17 @@
 # Third Party
 from allauth.account import signals
 
+# Squarelet
+from squarelet.oidc.middleware import send_cache_invalidations
+
 
 def email_confirmed(request, email_address, **kwargs):
     if email_address.primary:
-        pass
-        # XXX send cache invalidation for user
-        # sync.delay("User", "update", email_address.user.pk)
+        send_cache_invalidations("user", email_address.user.pk)
 
 
 def email_changed(request, user, from_email_address, to_email_address, **kwargs):
-    pass
-    # XXX send cache invalidation for user
-    # sync.delay("User", "update", user.pk)
+    send_cache_invalidations("user", user.pk)
 
 
 signals.email_confirmed.connect(
