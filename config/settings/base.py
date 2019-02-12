b995d8c441cda93fe3048736028e4e77b805771a
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-12 13:51:57 -0500

    settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -296,7 +296,6 @@ SQUARELET_URL = env("SQUARLET_URL", default="http://dev.squarelet.com")
 MUCKROCK_URL = env("MUCKROCK_URL", default="http://dev.muckrock.com")
 FOIAMACHINE_URL = env("FOIAMACHINE_URL", default="http://dev.foiamachine.com")
 DOCCLOUD_URL = env("DOCCLOUD_URL", default="http://dev.documentcloud.org")
-MUCKROCK_TOKEN = env("MUCKROCK_TOKEN", default="set-me")
 
 # stripe
 # ------------------------------------------------------------------------------
