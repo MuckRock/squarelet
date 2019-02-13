7070f4c7cbaff2d5807d74f44fe73212f95c92d4
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-12 21:35:16 -0500

    add debug toolbar urls

--- config/urls.py
+++ config/urls.py
@@ -60,7 +60,7 @@ if settings.DEBUG:
         ),
         path("500/", default_views.server_error),
     ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
+if "debug_toolbar" in settings.INSTALLED_APPS:
+    import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
