bdff490dddf16c522dca054e64fe91796e5f0bdb
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-22 10:10:15 -0500

    comments

--- squarelet/statistics/models.py
+++ squarelet/statistics/models.py
@@ -3,22 +3,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
-
-# XXX stats
-
-"""
-total users
-total users less agency users
-
-pro users count
-pro users for that day
-
-total org members
-total active orgs
-"""
-
-
 class Statistics(models.Model):
     """Nightly statistics"""
 
