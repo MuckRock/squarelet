a36788f757d3ebb47ad3b3f6e5066e6531673b2e
Mitchell Kotler <mitch@mitchellkotler.net>
2019-02-22 10:06:10 -0500

    add statistics to squarelet

--- squarelet/statistics/models.py
+++ squarelet/statistics/models.py
@@ -1,5 +1,7 @@
 # Django
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
@@ -15,3 +17,27 @@ pro users for that day
 total org members
 total active orgs
 """
+
+
+class Statistics(models.Model):
+    """Nightly statistics"""
+
+    # pylint: disable=invalid-name
+    date = models.DateField(unique=True)
+
+    total_users = models.IntegerField()
+    total_users_excluding_agencies = models.IntegerField()
+    total_users_pro = models.IntegerField()
+    total_users_org = models.IntegerField()
+
+    total_orgs = models.IntegerField()
+
+    users_today = models.ManyToManyField(User)
+    pro_users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return "Stats for %s" % self.date
+
+    class Meta:
+        ordering = ["-date"]
+        verbose_name_plural = "statistics"
