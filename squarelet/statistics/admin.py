842733a9cdb16499157375e9d483a5833225ae21
Mitchell Kotler <mitch@mitchellkotler.net>
2019-03-08 17:00:09 -0500

    fix up stats and admin

--- squarelet/statistics/admin.py
+++ squarelet/statistics/admin.py
@@ -1,4 +1,30 @@
 # Django
 from django.contrib import admin
 
-# Register your models here.
+# Third Party
+from reversion.admin import VersionAdmin
+
+# Local
+from .models import Statistics
+
+
+@admin.register(Statistics)
+class StatisticsAdmin(VersionAdmin):
+    list_display = (
+        "date",
+        "total_users",
+        "total_users_excluding_agencies",
+        "total_users_pro",
+        "total_users_org",
+        "total_orgs",
+    )
+    readonly_fields = (
+        "date",
+        "total_users",
+        "total_users_excluding_agencies",
+        "total_users_pro",
+        "total_users_org",
+        "total_orgs",
+        "users_today",
+        "pro_users",
+    )
