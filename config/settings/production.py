575555b7bc8bb822ccf81aca4b433ec9c8d916ce
Mitchell Kotler <mitch@mitchellkotler.net>
2019-03-30 13:16:01 -0400

    aws setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -87,6 +87,7 @@ _AWS_EXPIRY = 60 * 60 * 24 * 7
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
 }
+AWS_DEFAULT_ACL = "public-read"
 
 # STATIC
 # ------------------------
