da04d7fea68794c9eafbe992ecba5b5de1abed9a
Mitchell Kotler <mitch@mitchellkotler.net>
2019-04-04 09:56:21 -0400

    add some tests for user forms

--- squarelet/users/tests/conftest.py
+++ squarelet/users/tests/conftest.py
@@ -1,7 +1,11 @@
 # Third Party
 from pytest_factoryboy import register
 
+# Squarelet
+from squarelet.organizations.tests.factories import FreePlanFactory
+
 # Local
 from .factories import UserFactory
 
 register(UserFactory)
+register(FreePlanFactory)
---
+++ squarelet/users/tests/test_forms.py
@@ -0,0 +1,49 @@
+
+# Standard Library
+from unittest.mock import MagicMock
+
+# Third Party
+import pytest
+
+# Local
+from ..forms import SignupForm
+from ..models import User
+
+
+# pylint: disable=invalid-name
+
+
+@pytest.mark.django_db(transaction=True)
+def test_clean(free_plan_factory):
+    free_plan_factory()
+    data = {
+        "name": "john doe",
+        "username": "john",
+        "email": "doe@example.com",
+        "password1": "squarelet",
+        "stripe_pk": "key",
+        "plan": "free",
+    }
+    form = SignupForm(data)
+    assert form.is_valid()
+
+
+@pytest.mark.django_db(transaction=True)
+def test_save(rf, free_plan_factory):
+    free_plan_factory()
+    data = {
+        "name": "john doe",
+        "username": "john",
+        "email": "doe@example.com",
+        "password1": "squarelet",
+        "stripe_pk": "key",
+        "plan": "free",
+    }
+    request = rf.post("/accounts/signup/", data)
+    request.session = MagicMock()
+    form = SignupForm(data)
+    assert form.is_valid()
+    form.save(request)
+    assert User.objects.filter(
+        username=data["username"], email=data["email"], name=data["name"]
+    ).exists()
