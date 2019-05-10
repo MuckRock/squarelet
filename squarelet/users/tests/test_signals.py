caaa781696cb8ad22cbd850db1fa5bb7f2f3ddab
Mitchell Kotler <mitch@mitchellkotler.net>
2019-05-10 10:58:09 -0400

    tests

---
+++ squarelet/users/tests/test_signals.py
@@ -0,0 +1,40 @@
+
+
+# Standard Library
+from unittest.mock import Mock, PropertyMock
+
+# Third Party
+import pytest
+from allauth.account.models import EmailAddress
+
+# Squarelet
+from squarelet.users import signals
+
+
+def test_email_confirmed(user_factory, mocker):
+    mocked = mocker.patch("squarelet.users.signals.send_cache_invalidations")
+    user = user_factory.build()
+    email = EmailAddress(user=user, primary=True)
+    signals.email_confirmed(None, email)
+    mocked.assert_called_with("user", user.uuid)
+
+
+@pytest.mark.django_db(transaction=True)
+def test_email_changed(user_factory, mocker, mailoutbox):
+    mocked_cache_inv = mocker.patch("squarelet.users.signals.send_cache_invalidations")
+    mocked_customer = mocker.patch(
+        "squarelet.organizations.models.Organization.customer",
+        new_callable=PropertyMock,
+    )
+    user = user_factory(email_failed=True)
+    user.individual_organization.customer = mocked_customer
+    old_email = EmailAddress(email="old@example.com")
+    new_email = EmailAddress(email="new@example.com", user=user)
+    signals.email_changed(None, user, old_email, new_email)
+    assert mocked_customer().email == new_email.email
+    mocked_customer().save.assert_called_once()
+    assert not user.email_failed
+    mocked_cache_inv.assert_called_with("user", user.uuid)
+    mail = mailoutbox[0]
+    assert mail.subject == "Changed email address"
+    assert mail.to == [old_email.email]
