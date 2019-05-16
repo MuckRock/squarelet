5b151fc4295c327f0cdd880ec6e9d8708cff4db1
Mitchell Kotler <mitch@mitchellkotler.net>
2019-05-16 15:33:54 -0400

    statistic tests

---
+++ squarelet/statistics/tests/test_tasks.py
@@ -0,0 +1,19 @@
+
+# Standard Library
+from datetime import date, timedelta
+
+# Third Party
+import pytest
+
+# Local
+from .. import tasks
+from ..models import Statistics
+
+
+@pytest.mark.django_db()
+def test_store_statistics():
+    tasks.store_statistics()
+    stats = Statistics.objects.first()
+    assert stats.date == date.today() - timedelta(1)
+    assert stats.total_users == 0
+    assert stats.total_orgs == 0
