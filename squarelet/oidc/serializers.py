156704e4cbba838d085c6f7a61d83e772d44d952
Mitchell Kotler <mitch@mitchellkotler.net>
2019-10-24 16:21:57 -0400

    client api

---
+++ squarelet/oidc/serializers.py
@@ -0,0 +1,33 @@
+# Third Party
+from oidc_provider.models import Client
+from rest_framework import serializers
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = (
+            "id",
+            "name",
+            "owner",
+            "client_type",
+            "client_id",
+            "client_secret",
+            "date_created",
+            "website_url",
+            "terms_url",
+            "contact_email",
+            "logo",
+            "reuse_consent",
+            "redirect_uris",
+            "post_logout_redirect_uris",
+        )
+        extra_kwargs = {
+            "owner": {"read_only": True},
+            "client_id": {"read_only": True},
+            "client_secret": {"read_only": True},
+            "date_created": {"read_only": True},
+            "logo": {"required": False},
+            "redirect_uris": {"source": "_redirect_uris"},
+            "post_logout_redirect_uris": {"source": "_post_logout_redirect_uris"},
+        }
