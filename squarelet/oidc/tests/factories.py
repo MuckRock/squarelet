f067ee16a0c26063517ecd8714f95ee991cf610c
Mitchell Kotler <mitch@mitchellkotler.net>
2019-10-24 16:21:57 -0400

    client api

---
+++ squarelet/oidc/tests/factories.py
@@ -0,0 +1,23 @@
+# Standard Library
+from hashlib import sha224
+from random import randint
+from uuid import uuid4
+
+# Third Party
+import factory
+
+
+class ClientFactory(factory.django.DjangoModelFactory):
+    name = factory.Sequence(lambda n: f"Client {n}")
+    owner = factory.SubFactory("squarelet.users.tests.factories.UserFactory")
+    client_type = "confidential"
+    client_id = factory.LazyFunction(lambda: str(randint(1, 999999)).zfill(6))
+    client_secret = factory.LazyFunction(
+        lambda: sha224(uuid4().hex.encode()).hexdigest()
+    )
+    website_url = factory.Faker("url")
+    terms_url = factory.Faker("url")
+    contact_email = factory.Faker("email")
+
+    class Meta:
+        model = "oidc_provider.Client"
---
+++ squarelet/users/rules.py
@@ -0,0 +1,15 @@
+# pylint: disable=unused-argument, invalid-unary-operand-type
+
+# Third Party
+from rules import add_perm, always_deny, is_authenticated, predicate
+
+
+@predicate
+def is_me(user, user_):
+    return user == user_
+
+
+add_perm("users.view_user", is_authenticated)
+add_perm("users.add_user", always_deny)
+add_perm("users.change_user", is_authenticated & is_me)
+add_perm("users.delete_user", always_deny)
