c3f4354c9478eebc05b89ad581b0809858187ffb
R. Miles McCain <x1-key@sendmiles.email>
2019-11-01 14:56:11 -0400

    Add CSRF endpoint & modify debug cookie settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -68,3 +68,6 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:4200",
 ]
+
+CSRF_COOKIE_SAMESITE = None # Necessary for debugging
+SESSION_COOKIE_SAMESITE = None # Necessary for debugging
---
+++ squarelet/csrf/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+from django.middleware.csrf import get_token
+import json
+
+def get(request):
+    return JsonResponse({'csrfToken': get_token(request)})
+
+def ping(request):
+    print(request.META)
+    return JsonResponse({'result': 'OK'})
