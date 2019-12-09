96bdc64cda5ce5959f4ed00863c93b95280f77c5
R. Miles McCain <x1-key@sendmiles.email>
2019-12-09 10:41:58 -0500

    Format using black/isort

--- squarelet/users/viewsets.py
+++ squarelet/users/viewsets.py
@@ -31,6 +31,10 @@ from squarelet.users.serializers import (
     UserWriteSerializer,
 )
 
+# Local
+from .models import User
+from .serializers import UserReadSerializer, UserWriteSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.prefetch_related(
