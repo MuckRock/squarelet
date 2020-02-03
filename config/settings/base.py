e6d2c26ab41aec30de93d8bd3d3e6f4dbe392d8a
Mitchell Kotler <mitch@mitchellkotler.net>
2020-02-03 14:11:08 -0500

    tests for user api

--- config/settings/base.py
+++ config/settings/base.py
@@ -256,6 +256,34 @@ ADMINS = [("""Mitchell Kotler""", "mitch@muckrock.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
+# LOGGING
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
+if DEBUG:
+    LOGGING["loggers"] = {
+        "rules": {"handlers": ["console"], "level": "DEBUG", "propagate": True}
+    }
+
 # Celery
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ["squarelet.taskapp.celery.CeleryAppConfig"]
--- squarelet/users/tests/test_api.py
+++ squarelet/users/tests/test_api.py
@@ -4,7 +4,12 @@ from unittest.mock import Mock
 
 # Third Party
 import pytest
+from rest_framework import status
 from rest_framework.test import APIClient
+
+# Squarelet
+from squarelet.users.serializers import PressPassUserSerializer
+from squarelet.users.tests.factories import UserFactory
 
 
 @pytest.mark.django_db()
@@ -20,7 +25,7 @@ class TestUserAPI:
         # user `individual_organization_id` instead of `uuid` because
         # the `uuid` AliasField fails only in tests for some reason
         response = client.get(f"/api/users/{user.individual_organization_id}/")
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         response_json = json.loads(response.content)
         assert response_json["uuid"] == str(user.individual_organization_id)
         assert response_json["name"] == user.name
@@ -41,8 +46,59 @@ class TestUserAPI:
         client = APIClient()
         client.force_authenticate(user=user)
         response = client.post(f"/api/users/", data)
-        assert response.status_code == 201
+        assert response.status_code == status.HTTP_201_CREATED
         response_json = json.loads(response.content)
         assert response_json["name"] == data["name"]
         assert response_json["preferred_username"] == data["preferred_username"]
         assert response_json["email"] == data["email"]
+
+
+@pytest.mark.django_db()
+class TestPPUserAPI:
+    def test_list(self, api_client, user):
+        """List users"""
+        size = 10
+        api_client.force_authenticate(user=user)
+        UserFactory.create_batch(size)
+        response = api_client.get(f"/pp-api/users/")
+        assert response.status_code == status.HTTP_200_OK
+        response_json = json.loads(response.content)
+        assert len(response_json["results"]) == size + 1
+
+    def test_retrieve(self, api_client, user):
+        """Test retrieving a user"""
+        api_client.force_authenticate(user=user)
+        response = api_client.get(f"/pp-api/users/{user.individual_organization_id}/")
+        assert response.status_code == status.HTTP_200_OK
+        response_json = json.loads(response.content)
+
+    def test_retrieve_me(self, api_client, user):
+        """Test retrieving a user using special identifier `me`"""
+        api_client.force_authenticate(user=user)
+        response = api_client.get(f"/pp-api/users/me/")
+        assert response.status_code == status.HTTP_200_OK
+        response_json = json.loads(response.content)
+        serializer = PressPassUserSerializer(user)
+        assert response_json == serializer.data
+
+    def test_update(self, api_client, user):
+        """Test updating a user"""
+        api_client.force_authenticate(user=user)
+        name = "John Doe"
+        assert user.name != name
+        response = api_client.patch(
+            f"/pp-api/users/{user.individual_organization_id}/", {"name": name}
+        )
+        assert response.status_code == status.HTTP_200_OK
+        user.refresh_from_db()
+        assert user.name == name
+
+    def test_update_bad(self, api_client, user):
+        """Test updating a user you cannot update"""
+        other_user = UserFactory()
+        api_client.force_authenticate(user=user)
+        name = "John Doe"
+        response = api_client.patch(
+            f"/pp-api/users/{other_user.individual_organization_id}/", {"name": name}
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
