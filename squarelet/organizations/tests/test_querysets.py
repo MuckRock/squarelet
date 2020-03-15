b95cc419b9c58d1020f765b772afddc6f1050393
Jacqui Lough <lough.jacqui@gmail.com>
2020-03-16 09:22:05 +1100

    Added test that other users can't view private org memberships

--- squarelet/organizations/tests/test_querysets.py
+++ squarelet/organizations/tests/test_querysets.py
@@ -22,7 +22,10 @@ class TestMembershipQuerySet(TestCase):
     def test_get_viewable(self):
         admin, member, user = UserFactory.create_batch(3)
         private_org = OrganizationFactory(admins=[admin], private=True)
-        membership = MembershipFactory(organization=private_org, user=member)
+        MembershipFactory(organization=private_org, user=member)
 
         assert Membership.objects.get_viewable(member).count() == 3
         assert Membership.objects.get_viewable(user).count() == 1
+
+        another_user = UserFactory()
+        assert member.memberships.get_viewable(another_user).count() == 0
