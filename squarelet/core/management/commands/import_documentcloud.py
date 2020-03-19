66e5c1edf027b61ebe42bf03dc0dddf395f2e9b3
Mitchell Kotler <mitch@mitchellkotler.net>
2020-03-19 11:50:07 -0400

    import fixes

--- squarelet/core/management/commands/import_documentcloud.py
+++ squarelet/core/management/commands/import_documentcloud.py
@@ -8,6 +8,7 @@ import csv
 import os
 
 # Third Party
+import pytz
 from allauth.account.models import EmailAddress
 from dateutil.parser import parse
 from smart_open.smart_open_lib import smart_open
@@ -19,6 +20,7 @@ from squarelet.oidc.middleware import (
 )
 from squarelet.organizations.models import Membership, Organization, Plan
 from squarelet.users.models import User
+from squarelet.users.serializers import UserWriteSerializer
 
 BUCKET = os.environ["IMPORT_BUCKET"]
 
@@ -32,7 +34,9 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # pylint: disable=unused-argument
         org_id = kwargs["organization"]
-        self.bucket_path = f"s3://{BUCKET}/documentcloud-export/organization-{org_id}/"
+        self.bucket_path = (
+            f"s3://{BUCKET}/documentcloud-export-test/organization-{org_id}/"
+        )
         with transaction.atomic():
             # we initialize the cache invalidation set here in order to
             # not send a cache invalidation for users and orgs during the
@@ -51,13 +55,15 @@ class Command(BaseCommand):
             # we do not send the batched invalidations, but just delete them
             delete_cache_invalidation_set()
 
+            raise ValueError("Do not commit")
+
     def import_org(self):
         self.stdout.write("Begin Organization Import {}".format(timezone.now()))
         plan = Plan.objects.get(slug="free")
         with smart_open(
-            f"{self.bucket_path}organizations.csv", "rb"
+            f"{self.bucket_path}organizations.csv", "r"
         ) as infile, smart_open(
-            f"{self.bucket_path}organizations_map.csv", "wb"
+            f"{self.bucket_path}organizations_map.csv", "w"
         ) as outfile:
             reader = csv.reader(infile)
             writer = csv.writer(outfile)
@@ -79,8 +85,8 @@ class Command(BaseCommand):
                     individual=False,
                     private=fields[9] == "t",
                     verified_journalist=True,
-                    created_at=parse(fields[3]),
-                    updated_at=parse(fields[4]),
+                    created_at=parse(fields[3]).replace(tzinfo=pytz.UTC),
+                    updated_at=parse(fields[4]).replace(tzinfo=pytz.UTC),
                 )
             writer.writerow([fields[0], org.uuid])
         self.stdout.write("End Organization Import {}".format(timezone.now()))
@@ -88,8 +94,8 @@ class Command(BaseCommand):
 
     def import_users(self, organization):
         print("Begin User Import {}".format(timezone.now()))
-        with smart_open(f"{self.bucket_path}users.csv", "rb") as infile, smart_open(
-            f"{self.bucket_path}users_map.csv", "wb"
+        with smart_open(f"{self.bucket_path}users.csv", "r") as infile, smart_open(
+            f"{self.bucket_path}users_map.csv", "w"
         ) as outfile:
             reader = csv.reader(infile)
             writer = csv.writer(outfile)
@@ -113,7 +119,7 @@ class Command(BaseCommand):
                     created = True
                     self.stdout.write(f"Creating new user: {user[3]}")
                     user_obj = User.objects.create_user(
-                        username=UserWriteSerializer.unqiue_username(user[1] + user[2]),
+                        username=UserWriteSerializer.unique_username(user[1] + user[2]),
                         email=user[3],
                         name=f"{user[1]} {user[2]}",
                         is_staff=False,
@@ -123,8 +129,8 @@ class Command(BaseCommand):
                         is_agency=False,
                         use_autologin=True,
                         source="documentcloud",
-                        created_at=parse(user[5]),
-                        updated_at=parse(user[6]),
+                        created_at=parse(user[5]).replace(tzinfo=pytz.UTC),
+                        updated_at=parse(user[6]).replace(tzinfo=pytz.UTC),
                     )
                     user_obj.password = "bcrypt$" + user[4]
                     user_obj.save()
@@ -138,7 +144,7 @@ class Command(BaseCommand):
                         self.stdout.write(f"Adding to organization")
                         Membership.objects.create(
                             user=user_obj,
-                            organizationd=organization,
+                            organization=organization,
                             # 1 is admin
                             admin=user[10] == "1",
                         )
