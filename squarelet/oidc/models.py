d18f1551128af814a998d0c184fe2a7281549425
Tyler Fisher <tyler@tylerjfisher.com>
2020-04-07 12:47:06 -0400

    Switch OIDC styling based on new ClientProfile source field

--- squarelet/oidc/models.py
+++ squarelet/oidc/models.py
@@ -27,6 +27,15 @@ class ClientProfile(models.Model):
         blank=True,
         help_text=_("URL to send webhook notifications to for this client"),
     )
+    source = models.CharField(
+        _("source"),
+        max_length=10,
+        choices=(
+            ("muckrock", _("MuckRock")),
+            ("presspass", _("PressPass")),
+        ),
+        default="muckrock"
+    )
 
     def __str__(self):
         return str(self.client)
