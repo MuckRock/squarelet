88d3f5bcb20dc7c963221a6c895d0e8ff4186a6f
Jacqui Lough <lough.jacqui@gmail.com>
2020-04-30 12:12:17 +1000

    Limit clients api to admin-only users (+ test)

--- squarelet/oidc/tests/factories.py
+++ squarelet/oidc/tests/factories.py
@@ -9,7 +9,7 @@ import factory
 
 class ClientFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: f"Client {n}")
-    owner = factory.SubFactory("squarelet.users.tests.factories.UserFactory")
+    owner = factory.SubFactory("squarelet.users.tests.factories.UserFactory", is_staff=True)
     client_type = "confidential"
     client_id = factory.LazyFunction(lambda: str(randint(1, 999999)).zfill(6))
     client_secret = factory.LazyFunction(
