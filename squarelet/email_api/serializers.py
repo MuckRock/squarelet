36322e5f033ce3d280dbfdac12a4e6cb0564867e
Tyler Fisher <tyler@tylerjfisher.com>
2020-05-04 16:43:40 -0400

    Use proper validate function

--- squarelet/email_api/serializers.py
+++ squarelet/email_api/serializers.py
@@ -8,6 +8,27 @@ class PressPassEmailAddressSerializer(serializers.ModelSerializer):
         model = EmailAddress
         fields = ("email", "verified", "primary")
 
+    def validate(self, attrs):
+        request = self.context.get("request")
+        if request.data.get("action") == "update":
+            if (
+                not attrs["verified"]
+                and EmailAddress.objects.filter(
+                    user=request.user, verified=True
+                ).exists()
+            ):
+                raise serializers.ValidationError(
+                    "You can only make a verified email address primary."
+                )
+
+        if request.data.get("action") == "delete":
+            if request.data["primary"]:
+                raise serializers.ValidationError(
+                    "You cannot delete your primary email address."
+                )
+
+        return attrs
+
     def is_valid_update(self, attrs):
         request = self.context.get("request")
         if (
