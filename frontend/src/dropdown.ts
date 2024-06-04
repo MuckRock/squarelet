b2b1f3f7c98a15809c1fbfc693f0c83e26e34dd0
Chris Amico <eyeseast@gmail.com>
2024-06-04 15:07:09 -0400

    format everything

--- frontend/src/dropdown.ts
+++ frontend/src/dropdown.ts
@@ -1,13 +1,13 @@
-import {d, on, show, hide} from './util';
+import { d, on, show, hide } from "./util";
 
 export class DropdownView {
-  readonly dropdown = d('_id-profDropdown');
-  readonly contents = d('_id-dropdownContents');
-  readonly shim = d('_id-shim');
+  readonly dropdown = d("_id-profDropdown");
+  readonly contents = d("_id-dropdownContents");
+  readonly shim = d("_id-shim");
   hidden = true;
 
   constructor() {
-    on(this.dropdown, 'click', () => {
+    on(this.dropdown, "click", () => {
       if (this.hidden) {
         this.show();
       } else {
@@ -15,21 +15,21 @@ export class DropdownView {
       }
     });
 
-    on(this.shim, 'click', () => {
+    on(this.shim, "click", () => {
       this.hide();
     });
   }
 
   show() {
     this.hidden = false;
-    this.dropdown.classList.add('_cls-active');
+    this.dropdown.classList.add("_cls-active");
     show(this.contents);
     show(this.shim);
   }
 
   hide() {
     this.hidden = true;
-    this.dropdown.classList.remove('_cls-active');
+    this.dropdown.classList.remove("_cls-active");
     hide(this.contents);
     hide(this.shim);
   }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,17 @@
-import {AutocompleteView} from './autocomplete';
-import {exists} from './util';
-import {DropdownView} from './dropdown';
-import {EmailAddressView} from './emailaddress';
-import {ReceiptsView} from './receipts';
-import {PlansView} from './plans';
-import {ManageTableView} from './managetable';
+import { AutocompleteView } from "./autocomplete";
+import { exists } from "./util";
+import { DropdownView } from "./dropdown";
+import { EmailAddressView } from "./emailaddress";
+import { ReceiptsView } from "./receipts";
+import { PlansView } from "./plans";
+// import {ManageTableView} from './managetable';
 
-if (exists('_id-profDropdown')) {
+if (exists("_id-profDropdown")) {
   // Dropdown view;
   new DropdownView();
 }
 
-if (exists('_id-autocomplete')) {
+if (exists("_id-autocomplete")) {
   // Autocomplete page.
   new AutocompleteView();
 }
@@ -22,17 +22,17 @@ if (exists('_id-autocomplete')) {
 //   new ManageTableView();
 // }
 
-if (exists('_id-resendVerification')) {
+if (exists("_id-resendVerification")) {
   // E-mail address page.
   new EmailAddressView();
 }
 
-if (exists('_id-receiptsTable')) {
+if (exists("_id-receiptsTable")) {
   // Receipts page.
   new ReceiptsView();
 }
 
-if (exists('id_stripe_pk')) {
+if (exists("id_stripe_pk")) {
   // Stripe and plans pages.
   new PlansView();
 }
