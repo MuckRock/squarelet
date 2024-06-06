ed3b680a4f2e233090d07e9064348d2b262aab5f
Chris Amico <eyeseast@gmail.com>
2024-06-06 10:01:17 -0400

    add and remove comments

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,10 +175,8 @@ STATIC_ROOT = str(ROOT_DIR("staticfiles"))
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    str(APPS_DIR.path("static")),
     str(ROOT_DIR.path("frontend/dist")),
 ]
-
 
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
