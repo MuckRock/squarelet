9e9c866430eeb5089b3919e333e3c18fe3a30c56
Allan Lasser <lasser.allan@gmail.com>
2024-06-13 13:24:16 -0400

    Load data onto resource page

--- squarelet/core/views.py
+++ squarelet/core/views.py
@@ -71,3 +71,28 @@ class ERHLandingView(TemplateView):
                 context["resources"] = resources
                 context["categories"] = resource_categories(resources)
         return context
+
+class ERHResourceView(TemplateView):
+
+    template_name = "core/erh_resource.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        if self.request.user.is_authenticated:
+            context["can_access_hub"] = self.request.user.is_hub_eligible
+            context["eligible_orgs"] = self.request.user.organizations.filter(
+                Q(individual=False) &
+                (Q(hub_eligible=True)
+                 | Q(groups__hub_eligible=True)
+                 | Q(parent__hub_eligible=True))
+            )
+            context["group_orgs"] = self.request.user.organizations.filter(individual=False)
+            
+            if self.request.user.is_hub_eligible and 'id' in kwargs:
+                # get the resource
+                cache_key = 'erh_resource/{id}'.format(id=kwargs['id'])
+                resource = cache.get_or_set(cache_key, Resource.from_id(kwargs['id']), 300)
+                context['resource'] = resource
+                print(resource)
+        return context
+            
