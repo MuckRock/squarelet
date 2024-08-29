8c1ac76d181559d4ae1c416f49b71b6eaca59af4
Allan Lasser <lasser.allan@gmail.com>
2024-08-29 13:18:38 -0400

    More specific routing rules

--- config/urls.py
+++ config/urls.py
@@ -34,8 +34,8 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("election-hub/", ERHLandingView.as_view(), name="erh_landing"),
     path("election-hub/about", ERHAboutView.as_view(), name="erh_about"),
-    path("election-hub/<str:id>", ERHResourceView.as_view(), name="erh_resource"),
     path("election-hub/subscribe/", newsletter_subscription, name="newsletter_subscription"),
+    path("election-hub/<str:id>", ERHResourceView.as_view(), name="erh_resource"),
     path(
         "selectplan/",
         TemplateView.as_view(template_name="pages/selectplan.html"),
