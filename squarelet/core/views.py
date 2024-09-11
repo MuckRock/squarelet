a88fe0f6002e304dfd38c6cc1f6f733688e88a41
Allan Lasser <lasser.allan@gmail.com>
2024-09-11 15:00:04 -0400

    Update the conditions for resource visibility

--- squarelet/core/views.py
+++ squarelet/core/views.py
@@ -106,6 +106,10 @@ class ERHLandingView(TemplateView):
                 view="All Categories",
                 formula=match({"Status": "Published", "Show on Homepage": True}),
             )
+            # only include Accepted and Ready resources in the category
+            for category in categories:
+                category.resources = [resource for resource in category.resources if (
+                    resource.status == "Accepted" and resource.visible == "Ready")]
             cache.set(
                 "erh_homepage_categories", categories, settings.AIRTABLE_CACHE_TTL
             )
@@ -252,6 +256,10 @@ class ERHResourceView(TemplateView):
                 print("Cache miss. Fetching resource…")
                 resource = Resource.from_id(kwargs["id"])
                 cache.set(cache_key, resource, settings.AIRTABLE_CACHE_TTL)
+            # show the resource page if the status is accepted and the visibility is "Ready" or "Kondo"
+            show = resource.status == "Accepted" and (resource.visible in ["Ready", "Kondo"])
+            if not show:
+                raise Http404
             context["resource"] = resource
         except:
             raise Http404
