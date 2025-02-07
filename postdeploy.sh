7f1826b8c312e5b71e23c424002b3a37ce91af77
Allan Lasser <lasser.allan@gmail.com>
2025-02-07 14:47:01 -0500

    Add CLI buildpack, update command

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ -n "$HEROKU_APP_NAME" ]; then
-  heroku pg:copy squarelet-staging::DATABASE_URL DATABASE_URL --app $HEROKU_APP_NAME
+  heroku run "heroku pg:copy squarelet-staging::$DATABASE_URL $DATABASE_URL --app $HEROKU_APP_NAME --confirm $HEROKU_APP_NAME" -a $HEROKU_APP_NAME
 fi
