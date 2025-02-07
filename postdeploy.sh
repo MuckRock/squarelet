8f41cbe31399a00b6849fda9548e7bacfd0fb57a
Allan Lasser <lasser.allan@gmail.com>
2025-02-07 10:20:23 -0500

    Copy staging database into review app postdeploy

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Build static files to be collected"
-npm ci --include dev
-npm run build
-ls -la frontend/dist # debug
-
-echo "Setup Django"
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput -v 3 # verbose for debugging
+if [ -n "$HEROKU_APP_NAME" ]; then
+  heroku pg:copy squarelet-staging::DATABASE_URL DATABASE_URL --app $HEROKU_APP_NAME
+fi
