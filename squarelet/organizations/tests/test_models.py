972389e4406dee14459df709d263d3d88255d9aa
Mitchell Kotler <mitch@mitchellkotler.net>
2025-05-21 11:09:18 -0400

    fix test

--- squarelet/organizations/tests/test_models.py
+++ squarelet/organizations/tests/test_models.py
@@ -217,7 +217,7 @@ class TestOrganization:
         max_users = 10
         token = "token"
         organization.set_subscription(token, plan, max_users, user)
-        mocked.assert_called_with(token, plan, user)
+        mocked.assert_called_with(None, plan, user)
 
     @pytest.mark.django_db
     def test_set_subscription_cancel(
