3bf56ac68b21fccd63749703ec9e81ea737d65ac
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-06-17 16:48:40 -0500

    Moved method inside class

--- squarelet/statistics/admin.py
+++ squarelet/statistics/admin.py
@@ -12,43 +12,43 @@ from reversion.admin import VersionAdmin
 from .models import Statistics
 
 
-def export_statistics_as_csv(request, queryset):
-    """Export selected Statistics records to CSV."""
-    field_names = [
-        "date",
-        "total_users",
-        "total_users_excluding_agencies",
-        "total_users_pro",
-        "total_users_org",
-        "total_orgs",
-    ]
-
-    response = HttpResponse(content_type="text/csv")
-    response["Content-Disposition"] = "attachment; filename=squarelet_statistics.csv"
-
-    writer = csv.writer(response)
-    writer.writerow(field_names)
-
-    for obj in queryset:
-        writer.writerow(
-            [
-                obj.date.isoformat(),
-                obj.total_users,
-                obj.total_users_excluding_agencies,
-                obj.total_users_pro,
-                obj.total_users_org,
-                obj.total_orgs,
-            ]
-        )
-
-    return response
-
-
-export_statistics_as_csv.short_description = "Export selected statistics to CSV"
-
-
 @admin.register(Statistics)
 class StatisticsAdmin(VersionAdmin):
+    def export_statistics_as_csv(self, request, queryset):
+        """Export selected Statistics records to CSV."""
+        field_names = [
+            "date",
+            "total_users",
+            "total_users_excluding_agencies",
+            "total_users_pro",
+            "total_users_org",
+            "total_orgs",
+        ]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = (
+            "attachment; filename=squarelet_statistics.csv"
+        )
+
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+
+        for obj in queryset:
+            writer.writerow(
+                [
+                    obj.date.isoformat(),
+                    obj.total_users,
+                    obj.total_users_excluding_agencies,
+                    obj.total_users_pro,
+                    obj.total_users_org,
+                    obj.total_orgs,
+                ]
+            )
+
+        return response
+
+    export_statistics_as_csv.short_description = "Export selected statistics to CSV"
+
     list_display = (
         "date",
         "total_users",
