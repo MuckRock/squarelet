9fcc45ad0dea896c2447d4e25aecd95649d6c0f7
Allan Lasser <lasser.allan@gmail.com>
2025-07-21 20:33:39 -0400

    Adds filter for org privacy

--- squarelet/organizations/filters.py
+++ squarelet/organizations/filters.py
@@ -8,7 +8,8 @@ from .models import Organization
 class OrganizationFilter(filters.FilterSet):
     verified = filters.BooleanFilter(field_name="verified_journalist")
     individual = filters.BooleanFilter(field_name="individual")
+    private = filters.BooleanFilter(field_name="private")
 
     class Meta:
         model = Organization
-        fields = ["verified", "individual"]
+        fields = ["verified", "individual", "private"]
