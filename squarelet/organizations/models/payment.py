fa6a9dc3dde8df0eddf364ebca9c284e0f50ba22
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-08-25 01:42:56 -0500

    Format again

--- squarelet/organizations/models/payment.py
+++ squarelet/organizations/models/payment.py
@@ -154,7 +154,7 @@ class Subscription(models.Model):
     def send_notification(self, subject, message):
         """Queue a Slack notification asynchronously."""
         if self.plan.slack_webhook_url:
-            from squarelet.organizations.tasks import (# pylint:disable=import-outside-toplevel
+            from squarelet.organizations.tasks import (  # pylint:disable=import-outside-toplevel
                 send_slack_notification,
             )
 
