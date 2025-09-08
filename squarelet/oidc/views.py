4dbb40b3a28688233132ad238432f5bf98ee4649
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2025-09-08 13:37:42 -0500

    Fix import

--- squarelet/oidc/views.py
+++ squarelet/oidc/views.py
@@ -16,6 +16,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
+# Squarelet
+from squarelet.oidc.permissions import ScopePermission
+
 
 class TokenEndpoint(endpoints.token.TokenEndpoint):
     """Override OIDC token endpoint to use our JWT tokens"""
