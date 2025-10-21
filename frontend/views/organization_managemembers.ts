5751764ed2957be1ca03bd039113840e07eadf95
Chris Amico <eyeseast@gmail.com>
2025-10-21 15:50:49 -0400

    Back to invitation_list_item.html

--- frontend/views/organization_managemembers.ts
+++ frontend/views/organization_managemembers.ts
@@ -1,4 +1,4 @@
 /* for /organizations/<slug>/manage-members/ */
 import "@/css/organization_managemembers.css";
 import "@/css/user_list_item.css";
-// import "@/css/invitation_list_item.css";
+import "@/css/invitation_list_item.css";
