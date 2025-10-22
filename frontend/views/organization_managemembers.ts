c1784707298e970460573a752cda202940b480b8
Chris Amico <eyeseast@gmail.com>
2025-10-22 12:16:06 -0400

    Use a text field to show links and add copy text function

--- frontend/views/organization_managemembers.ts
+++ frontend/views/organization_managemembers.ts
@@ -2,3 +2,18 @@
 import "@/css/organization_managemembers.css";
 import "@/css/user_list_item.css";
 import "@/css/invitation_list_item.css";
+
+function init() {
+  const buttons = document.querySelectorAll("button.copy");
+
+  buttons.forEach((buttton) => {
+    buttton.addEventListener("click", (e) => {
+      const link = buttton.getAttribute("value");
+      if (link) {
+        window?.navigator?.clipboard?.writeText(link);
+      }
+    });
+  });
+}
+
+window.addEventListener("DOMContentLoaded", init);
