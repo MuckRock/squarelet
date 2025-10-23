61e0c92ee9263f3b29f326a65f4609a481dc0c96
Mitchell Kotler <mitch@mitchellkotler.net>
2025-10-23 14:56:46 -0400

    add 2fa status for staff to digest

--- squarelet/users/mail.py
+++ squarelet/users/mail.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import Group
 # Standard Library
 from datetime import date
 
+# Third Party
+from allauth.mfa.utils import is_mfa_enabled
+
 # Squarelet
 from squarelet.core.mail import Email
 from squarelet.users.models import User
@@ -22,9 +25,11 @@ class PermissionsDigest(Email):
         super().__init__(**kwargs)
 
     def get_context(self):
+        staff = User.objects.filter(is_staff=True)
+        staff = [(u, is_mfa_enabled(u)) for u in staff]
         return {
             "superusers": User.objects.filter(is_superuser=True),
-            "staff": User.objects.filter(is_staff=True),
+            "staff": staff,
             "groups": Group.objects.prefetch_related("user_set"),
             "user_permissions": User.user_permissions.through.objects.select_related(
                 "user",
