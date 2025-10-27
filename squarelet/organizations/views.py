e04338df14361162ebdf3139d3bc23ced3af7495
Allan Lasser <lasser.allan@gmail.com>
2025-10-27 10:23:05 -0400

    Clean up `pluralize`

--- squarelet/organizations/views.py
+++ squarelet/organizations/views.py
@@ -46,7 +46,7 @@ from squarelet.core.utils import (
     get_redirect_url,
     get_stripe_dashboard_url,
     is_rate_limited,
-    pluralize
+    pluralize,
 )
 from squarelet.organizations.choices import ChangeLogReason
 from squarelet.organizations.denylist_domains import DENYLIST_DOMAINS
