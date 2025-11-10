741926062e1a9b9f8f27f01ca7ff49892386bb61
Chris Amico <eyeseast@gmail.com>
2025-11-10 14:10:53 -0500

    Migration required! Add 'about' field to Organization model.

--- squarelet/organizations/admin.py
+++ squarelet/organizations/admin.py
@@ -306,6 +306,7 @@ class OrganizationAdmin(VersionAdmin):
         "created_at",
         "updated_at",
         "avatar",
+        "about",
         "individual",
         "private",
         "verified_journalist",
