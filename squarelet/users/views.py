b0b56c002c4e8c520d51c81a8d79ca2fa977bfe2
Allan Lasser <lasser.allan@gmail.com>
2025-11-13 10:07:03 -0500

    Adds routes for showing org invitations and requests for a single user

--- squarelet/users/views.py
+++ squarelet/users/views.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import models
 from django.http.response import (
     Http404,
     HttpResponse,
@@ -47,7 +48,7 @@ from allauth.socialaccount.views import ConnectionsView
 # Squarelet
 from squarelet.core.mixins import AdminLinkMixin
 from squarelet.core.utils import new_action
-from squarelet.organizations.models import ReceiptEmail
+from squarelet.organizations.models import Invitation, ReceiptEmail
 from squarelet.organizations.models.payment import Plan
 from squarelet.organizations.views import UpdateSubscription
 from squarelet.services.models import Service
@@ -519,3 +520,156 @@ class UserPaymentView(LoginRequiredMixin, StaffAccessMixin, UpdateSubscription):
         username = self.kwargs.get("username")
         target_user = User.objects.get(username=username)
         return target_user.individual_organization
+
+# TODO: Consolidate UserInvitationsView and UserRequestsView since they are very similar
+class UserInvitationsView(LoginRequiredMixin, StaffAccessMixin, ListView):
+    """View to display all invitations received by a user"""
+
+    model = Invitation
+    template_name = "users/user_invitations.html"
+    context_object_name = "invitations"
+    paginate_by = 20
+
+    def get_queryset(self):
+        """Get all invitations (request=False) for the user's verified emails"""
+        username = self.kwargs.get("username")
+        user = User.objects.get(username=username)
+        verified_emails = user.get_verified_emails()
+
+        if not verified_emails:
+            return Invitation.objects.none()
+
+        # Get all invitations (not requests) for this user's verified emails
+        # Include user field for cases where the user was directly invited
+        # TODO: Refact this into a queryset method on Invitation
+        queryset = (
+            Invitation.objects.filter(
+                models.Q(email__in=verified_emails) | models.Q(user=user), request=False
+            )
+            .select_related("organization")
+            .order_by("-created_at")
+        )
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.kwargs.get("username")
+        target_user = User.objects.get(username=username)
+        context["target_user"] = target_user
+        context["is_own_page"] = target_user == self.request.user
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """Handle undo action for rejected invitations"""
+        invitation_uuid = request.POST.get("invitation_uuid")
+        action = request.POST.get("action")
+
+        # TODO: Clarify action is sending a new request, not undoing a rejected invite
+        if action == "undo_reject":
+            try:
+                # Get the rejected invitation
+                invitation = Invitation.objects.get(
+                    uuid=invitation_uuid, rejected_at__isnull=False
+                )
+
+                # Create a new request to join the organization
+                new_request = Invitation.objects.create(
+                    organization=invitation.organization,
+                    user=request.user,
+                    email=request.user.email,
+                    request=True,
+                )
+                new_request.send()
+
+                messages.success(
+                    request,
+                    _(
+                        f"Request to join {invitation.organization.name} "
+                        "has been sent."
+                    ),
+                )
+            except Invitation.DoesNotExist:
+                messages.error(
+                    request, _("Invitation not found or has not been rejected.")
+                )
+
+        return HttpResponseRedirect(
+            reverse("users:invitations", kwargs={"username": request.user.username})
+        )
+
+
+class UserRequestsView(LoginRequiredMixin, StaffAccessMixin, ListView):
+    """View to display all requests sent by a user"""
+
+    model = Invitation
+    template_name = "users/user_requests.html"
+    context_object_name = "requests"
+    paginate_by = 20
+
+    def get_queryset(self):
+        """Get all requests (request=True) sent by the user"""
+        username = self.kwargs.get("username")
+        user = User.objects.get(username=username)
+        verified_emails = user.get_verified_emails()
+
+        if not verified_emails:
+            return Invitation.objects.none()
+
+        # Get all requests for this user's verified emails or user field
+        # TODO: Refact this into a queryset method on Invitation
+        queryset = (
+            Invitation.objects.filter(
+                models.Q(email__in=verified_emails) | models.Q(user=user), request=True
+            )
+            .select_related("organization")
+            .order_by("-created_at")
+        )
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.kwargs.get("username")
+        target_user = User.objects.get(username=username)
+        context["target_user"] = target_user
+        context["is_own_page"] = target_user == self.request.user
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """Handle undo action for rejected requests"""
+        invitation_uuid = request.POST.get("invitation_uuid")
+        action = request.POST.get("action")
+
+        # TODO: Users cannot undo a request rejected by someone else
+        if action == "undo_reject":
+            try:
+                # Get the rejected request
+                old_request = Invitation.objects.get(
+                    uuid=invitation_uuid, rejected_at__isnull=False
+                )
+
+                # Create a new request to join the organization
+                new_request = Invitation.objects.create(
+                    organization=old_request.organization,
+                    user=request.user,
+                    email=request.user.email,
+                    request=True,
+                )
+                new_request.send()
+
+                messages.success(
+                    request,
+                    _(
+                        f"New request to join {old_request.organization.name} "
+                        "has been sent."
+                    ),
+                )
+            except Invitation.DoesNotExist:
+                messages.error(
+                    request, _("Request not found or has not been rejected.")
+                )
+
+        return HttpResponseRedirect(
+            reverse("users:requests", kwargs={"username": request.user.username})
+        )
