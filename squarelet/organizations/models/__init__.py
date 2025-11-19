a61e4e31f58fdec9bdb7a83bd02b91b3e1f1d033
Chris Amico <eyeseast@gmail.com>
2025-11-19 16:39:49 -0500

    Add zendesk integration and split off change request model into its own file

--- squarelet/organizations/models/__init__.py
+++ squarelet/organizations/models/__init__.py
@@ -3,3 +3,4 @@ from squarelet.organizations.models.invoice import *
 from squarelet.organizations.models.organization import *
 from squarelet.organizations.models.organization_metadata import *
 from squarelet.organizations.models.payment import *
+from squarelet.organizations.models.profile_change_request import *
