5c5352083b2e1447862e854dd90524b01917bc3c
Allan Lasser <lasser.allan@gmail.com>
2025-12-04 11:27:11 -0500

    Sort imports

--- squarelet/organizations/tests/models/test_invoice.py
+++ squarelet/organizations/tests/models/test_invoice.py
@@ -13,6 +13,7 @@ import stripe
 from squarelet.organizations.models import Invoice
 
 
+
 def create_mock_stripe_invoice(invoice_id, amount_due, status, created, due_date=None):
     """
     Create a mock Stripe invoice object that supports dictionary-style access.
