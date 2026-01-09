0743d494b2c6394aa61e1800f5fdc9c987d053e9
Chris Amico <eyeseast@gmail.com>
2026-01-09 09:38:05 -0500

    Split organizations/views.py into smaller files

---
+++ squarelet/organizations/views/members.py
@@ -0,0 +1,237 @@
+# Django
+from django.conf import settings
+from django.contrib import messages
+from django.http.response import HttpResponseBadRequest
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.html import format_html
+from django.views.generic import DetailView
+
+# Squarelet
+from squarelet.core.utils import (
+    get_redirect_url,
+    pluralize,
+)
+from squarelet.organizations.forms import AddMemberForm
+from squarelet.organizations.mixins import OrganizationAdminMixin
+from squarelet.organizations.models import (
+    Invitation,
+    Membership,
+    Organization,
+)
+
+
+class ManageMembers(OrganizationAdminMixin, DetailView):
+    queryset = Organization.objects.filter(individual=False)
+    template_name = "organizations/organization_managemembers.html"
+
+    def post(self, request, *args, **kwargs):
+        """Handle form processing"""
+        self.organization = self.get_object()
+
+        actions = {
+            "addmember": self._handle_add_member,
+            "addmember_link": self._handle_add_member_link,
+            "revokeinvite": self._handle_revoke_invite,
+            "resendinvite": self._handle_resend_invite,
+            "acceptinvite": self._handle_accept_invite,
+            "rejectinvite": self._handle_reject_invite,
+            "makeadmin": self._handle_makeadmin_user,
+            "removeuser": self._handle_remove_user,
+        }
+        try:
+            return actions[request.POST["action"]](request)
+        except KeyError:
+            return self._bad_call(request)
+
+    def _handle_add_member(self, request):
+        addmember_form = AddMemberForm(request.POST)
+        if not addmember_form.is_valid():
+            messages.error(
+                request, addmember_form.errors.get("emails", ["Invalid input."])[0]
+            )
+        else:
+            emails = addmember_form.cleaned_data["emails"]
+            invitations_sent = 0
+            for email in emails:
+                is_already_member = self.organization.has_member_by_email(email)
+                existing_open_invite = self.organization.get_existing_open_invite(email)
+                if is_already_member:
+                    messages.info(
+                        self.request,
+                        f"{email} is already a member of this organization.",
+                    )
+                    continue
+
+                if existing_open_invite:
+                    existing_open_invite.send()
+                    messages.success(
+                        self.request,
+                        f"Resent invitation to {email}.",
+                    )
+                    continue
+
+                invitation = Invitation.objects.create(
+                    organization=self.organization,
+                    email=email,
+                )
+                invitation.send()
+                invitations_sent += 1
+            messages.success(
+                self.request,
+                f"{invitations_sent} {pluralize(invitations_sent, 'invitation')} sent",
+            )
+
+        return redirect("organizations:manage-members", slug=self.organization.slug)
+
+    def _handle_add_member_link(self, request):
+        # Create an invitation and display it to the admin
+        invitation = Invitation.objects.create(
+            organization=self.organization,
+        )
+        url = reverse("organizations:invitation", args=(invitation.uuid,))
+        messages.success(
+            self.request,
+            format_html(
+                "Invitation link created:<p>{}{}</p>", settings.SQUARELET_URL, url
+            ),
+        )
+        return redirect("organizations:manage-members", slug=self.organization.slug)
+
+    def _handle_invite(self, request, invite_fn, success_message_fn):
+        try:
+            inviteid = request.POST.get("inviteid")
+            invite = Invitation.objects.get_open().get(
+                pk=inviteid, organization=self.organization
+            )
+            invite_fn(invite)
+            messages.success(self.request, success_message_fn(invite))
+        except Invitation.DoesNotExist:
+            return self._bad_call(request)
+        return redirect("organizations:manage-members", slug=self.organization.slug)
+
+    def _handle_revoke_invite(self, request):
+        return self._handle_invite(
+            request,
+            lambda invite: invite.reject(),
+            lambda invite: f"Invitation to {invite.email} revoked",
+        )
+
+    def _handle_resend_invite(self, request):
+        return self._handle_invite(
+            request,
+            lambda invite: invite.send(),
+            lambda invite: f"Invitation to {invite.email} resent successfully.",
+        )
+
+    def _handle_accept_invite(self, request):
+        return self._handle_invite(
+            request,
+            lambda invite: invite.accept(),
+            lambda invite: f"Invitation from {invite.email} accepted",
+        )
+
+    def _handle_reject_invite(self, request):
+        return self._handle_invite(
+            request,
+            lambda invite: invite.reject(),
+            lambda invite: f"Invitation from {invite.email} rejected",
+        )
+
+    def _handle_user(self, request, membership_fn, success_message_fn):
+        try:
+            userid = request.POST.get("userid")
+            membership = self.organization.memberships.get(user_id=userid)
+            membership_fn(membership)
+            messages.success(self.request, success_message_fn(membership))
+        except Membership.DoesNotExist:
+            return self._bad_call(request)
+        return redirect("organizations:manage-members", slug=self.organization.slug)
+
+    def _handle_makeadmin_user(self, request):
+        admin_param = request.POST.get("admin")
+        if admin_param == "true":
+            set_admin = True
+        elif admin_param == "false":
+            set_admin = False
+        else:
+            return self._bad_call(request)
+
+        def handle_make_admin(membership):
+            membership.admin = set_admin
+            membership.save()
+
+        return self._handle_user(
+            request,
+            handle_make_admin,
+            lambda membership: (
+                f"{membership.user.username} promoted to admin"
+                if set_admin
+                else f"{membership.user.username} demoted to member"
+            ),
+        )
+
+    def _handle_remove_user(self, request):
+        return self._handle_user(
+            request,
+            lambda membership: membership.delete(),
+            lambda membership: f"{membership.user.username} removed",
+        )
+
+    def _bad_call(self, request):
+        messages.error(self.request, "An unexpected error occurred")
+        return redirect("organizations:manage-members", slug=self.organization.slug)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["admin"] = self.request.user
+        context["members"] = list(
+            self.object.memberships.select_related("user").order_by("user__created_at")
+        )
+        context["requested_invitations"] = list(
+            self.object.invitations.get_pending_requests()
+        )
+        context["pending_invitations"] = list(
+            self.object.invitations.get_pending_invitations()
+        )
+        return context
+
+
+class InvitationAccept(DetailView):
+    queryset = Invitation.objects.get_pending()
+    slug_field = "uuid"
+    slug_url_kwarg = "uuid"
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        If the user is authenticated, associate the invitation with that user, so
+        they can access it later.
+        If not, store the invitation in the session, so that the invitation can be
+        associated on login
+        """
+        if request.user.is_authenticated:
+            Invitation.objects.filter(uuid=kwargs["uuid"]).update(user=request.user)
+        else:
+            request.session["invitation"] = str(kwargs["uuid"])
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """Accept the invitation"""
+        if not request.user.is_authenticated:
+            return HttpResponseBadRequest()
+        invitation = self.get_object()
+        action = request.POST.get("action")
+        if action == "accept":
+            invitation.accept(request.user)
+            messages.success(request, "Invitation accepted")
+            return get_redirect_url(request, redirect(invitation.organization))
+        elif action == "reject":
+            invitation.reject()
+            if invitation.request:
+                messages.info(request, "Invitation withdrawn")
+            else:
+                messages.info(request, "Invitation rejected")
+            return get_redirect_url(request, redirect(request.user))
+        else:
+            messages.error(request, "Invalid choice")
+            return get_redirect_url(request, redirect(request.user))
