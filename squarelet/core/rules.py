bca6acef4d74c758142964418417ede654dca4b6
Allan Lasser <lasser.allan@gmail.com>
2026-02-19 17:30:50 -0500

    Update permissions to use db+object checks

--- squarelet/core/rules.py
+++ squarelet/core/rules.py
@@ -1,6 +1,12 @@
 # Standard Library
 from functools import wraps
 
+# Django
+from django.contrib.auth import load_backend
+
+# Third Party
+from rules import add_perm, predicate
+
 
 def skip_if_not_obj(func):
     """Decorator for predicates
@@ -16,15 +22,20 @@ def skip_if_not_obj(func):
     return inner
 
 
-def deny_if_not_obj(func):
-    """Decorator for predicates
-    Deny the predicate if obj is None"""
-
-    @wraps(func)
-    def inner(user, obj):
-        if obj is None:
-            return False
-        else:
-            return func(user, obj)
+def has_db_perm(perm):  
+    @predicate(f"has_db_perm:{perm}")  
+    def inner(user):  
+        # we want to directly check the model backend for a permissions to avoid  
+        # infinite recursion  
+        backend = load_backend("django.contrib.auth.backends.ModelBackend")  
+        return backend.has_perm(user, perm)
 
     return inner
+
+
+def add_perm_with_db_check(perm, pred):  
+    """  
+    This will give permissions if the `pred` passes or if  
+    the permission has been explicitly set  
+    """  
+    add_perm(perm, pred | has_db_perm(perm))  
