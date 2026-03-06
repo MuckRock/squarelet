2f7d79b4628aaa125d939c572d3c65ab37b2f803
Allan Lasser <lasser.allan@gmail.com>
2026-03-06 17:12:29 -0500

    Adds security section to organization detail page

--- frontend/views/organization_detail.ts
+++ frontend/views/organization_detail.ts
@@ -1,4 +1,4 @@
 import "../css/sidebar_layout.css";
 import "../css/user_list_item.css";
-import "../css/organization_detail.css";
 import "../css/user_detail.css";
+import "../css/organization_detail.css";
