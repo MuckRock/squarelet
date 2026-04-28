76f127400132e69fae443d094e57718c58ef22e6
Allan Lasser <lasser.allan@gmail.com>
2026-04-27 21:30:49 -0400

    Adds "Will Renew" column when filtering orgs by plan

--- squarelet/organizations/admin.py
+++ squarelet/organizations/admin.py
@@ -1,6 +1,6 @@
 # Django
 from django.contrib import admin, messages
-from django.db.models import Count, JSONField, Q, Sum
+from django.db.models import Count, Exists, JSONField, OuterRef, Q, Sum
 from django.forms.models import BaseInlineFormSet
 from django.forms.widgets import Textarea
 from django.http.response import HttpResponse
@@ -325,6 +325,14 @@ class OrganizationAdmin(VersionAdmin):
         "get_subtypes",
         "get_outstanding_invoices",
     )
+
+    def get_list_display(self, request):
+        """Add cancelled column when filtering by plan"""
+        list_display = list(super().get_list_display(request))
+        if "plan" in request.GET and request.GET["plan"] != "none":
+            list_display.append("get_subscription_renews")
+        return list_display
+
     list_filter = (
         PlanFilter,
         "individual",
@@ -394,7 +402,7 @@ class OrganizationAdmin(VersionAdmin):
     )
 
     def get_queryset(self, request):
-        return (
+        qs = (
             super()
             .get_queryset(request)
             .prefetch_related("subtypes", "domains")
@@ -407,6 +415,18 @@ class OrganizationAdmin(VersionAdmin):
                 ),
             )
         )
+        plan_value = request.GET.get("plan")
+        if plan_value and plan_value != "none":
+            qs = qs.annotate(
+                subscription_cancelled=Exists(
+                    Subscription.objects.filter(
+                        organization_id=OuterRef("pk"),
+                        plan_id=plan_value,
+                        cancelled=True,
+                    )
+                )
+            )
+        return qs
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
@@ -457,6 +477,16 @@ class OrganizationAdmin(VersionAdmin):
 
     get_outstanding_invoices.short_description = "Outstanding Invoices"
     get_outstanding_invoices.admin_order_field = "outstanding_invoice_count"
+
+    def get_subscription_renews(self, obj):
+        cancelled = getattr(obj, "subscription_cancelled", None)
+        if cancelled is None:
+            return "-"
+        return not cancelled
+
+    get_subscription_renews.short_description = "Will Renew"
+    get_subscription_renews.boolean = True
+    get_subscription_renews.admin_order_field = "subscription_cancelled"
 
 
 @admin.register(Plan)
