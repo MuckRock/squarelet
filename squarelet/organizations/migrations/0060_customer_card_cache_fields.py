696325e8bc8ff48b6c634163e409f5c7f3288874
Mitchell Kotler <mitch@mitchellkotler.net>
2026-07-16 12:35:52 -0400

    fix imports

--- squarelet/organizations/migrations/0060_customer_card_cache_fields.py
+++ squarelet/organizations/migrations/0060_customer_card_cache_fields.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="customer",
             name="payment_brand",
-            field=models.CharField(blank=True, default="", max_length=20),
+            field=models.CharField(blank=True, default="", max_length=64),
         ),
         migrations.AddField(
             model_name="customer",
