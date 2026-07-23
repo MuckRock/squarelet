48c9e5883d78e50733bf460077c6552f8897c243
Daniel Nass <dnlnss@gmail.com>
2026-07-23 16:03:29 -0400

    Use CSS to toggle message visibility

--- frontend/views/organization_leave.ts
+++ frontend/views/organization_leave.ts
@@ -7,21 +7,11 @@ window.addEventListener("DOMContentLoaded", () => {
   const el = document.getElementById("user-select");
   if (!el) return;
 
-  const form = el.closest("form");
-  const submitMessage = form.querySelector("button[type=submit] .message");
-
   mount(OrgUserSelect, {
     target: el,
     props: {
       organizationSlug: el.dataset.orgSlug,
       organizationName: el.dataset.orgName,
-      onChange(user) {
-        if (user) {
-          submitMessage.textContent = "Assign and leave";
-        } else {
-          submitMessage.textContent = "Leave without assigning";
-        }
-      },
     },
   });
 });
