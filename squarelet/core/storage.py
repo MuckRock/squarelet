8605858d9a6559675100cce105ab4f03a752776b
Mitchell Kotler <mitch@mitchellkotler.net>
2026-07-23 14:22:01 -0400

    use private storage for receipts

--- squarelet/core/storage.py
+++ squarelet/core/storage.py
@@ -1,3 +1,7 @@
+# Django
+from django.conf import settings
+from django.core.files.storage import default_storage
+
 # Third Party
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -7,3 +11,22 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaRootS3BotoStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+
+
+class PrivateMediaStorage(S3Boto3Storage):
+    location = "media"
+    default_acl = "private"
+    file_overwrite = False
+    querystring_auth = True
+
+
+def private_storage():
+    """Return PrivateMediaStorage in production, default storage
+    otherwise.
+
+    Using a callable avoids instantiating S3 storage in environments
+    where AWS credentials are not configured (local dev, tests).
+    """
+    if hasattr(settings, "AWS_STORAGE_BUCKET_NAME"):
+        return PrivateMediaStorage()
+    return default_storage
